173d8523f641fd1be97462f82ba9d39f9298c882
Senrathy <sergioricardort@hotmail.com>
2022-12-18 20:54:59 -0300

    Solving problems

--- lib/features/contacts/list/contacts_list_page.dart
+++ lib/features/contacts/list/contacts_list_page.dart
@@ -14,6 +14,7 @@ class ContactsListPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await Navigator.pushNamed(context, '/contacts/register');
+          // ignore: use_build_context_synchronously
           context.read<ContactListBloc>().add(const ContactListEvent.findAll());
         },
         child: const Icon(Icons.add),
