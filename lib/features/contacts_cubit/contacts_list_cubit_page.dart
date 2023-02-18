aa551331380ae55e94b3480dc28c8c4ba413665e
Senrathy <sergioricardort@hotmail.com>
2023-02-17 21:47:36 -0300

    Started contacts list cubit page

--- lib/features/contacts_cubit/contacts_list_cubit_page.dart
+++ lib/features/contacts_cubit/contacts_list_cubit_page.dart
@@ -1,14 +1,35 @@
+import 'package:contact_bloc/features/contacts_cubit/list/cubit/contacts_list_cubit.dart';
 import 'package:flutter/material.dart';
 
+import '../../widgets/loader.dart';
+
 class ContactsListCubitPage extends StatelessWidget {
+  const ContactsListCubitPage({super.key});
 
-  const ContactsListCubitPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('List Cubit'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List Cubit'),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Column(
+              children: [
+                Loader<ContactsListCubit, ContactsListCubitState>(
+                  selector: (state) {
+                    return state.maybeWhen(
+                      orElse: () => false,
+                      loading: () => true,
+                    );
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
